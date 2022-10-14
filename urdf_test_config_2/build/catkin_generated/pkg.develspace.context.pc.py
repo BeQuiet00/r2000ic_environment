84551ed7cfbf2f4ff4f747db92e87bafb57857ec
Tim Han <tianyiha@andrew.cmu.edu>
2022-10-13 23:08:40 -0400

    create configs from moveit setup assistant

---
+++ urdf_test_config_2/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "urdf_test_config_2"
+PROJECT_SPACE_DIR = "/home/cerlab/Tim/r2000ic_environment/urdf_test_config_2/build/devel"
+PROJECT_VERSION = "0.3.0"
